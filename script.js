7bf4cee6b3e1924387fb03d9228407a642ef0336
Mfoniso_Ukpabio <ukpabiomfoniso@gmail.com>
2022-04-10 03:02:48 +0100

    Activated SPA

--- script.js
+++ script.js
@@ -161,15 +161,103 @@ function consumeAPI() {
       console.error(error.name);
       console.error(error.message);
     });
-    
-    // USING ASYNC AND AWAIT FOR SAME CODE
 
-    // async function catchPost() {
-    //   const response = await fetch("https://jsonplaceholder.typicode.com/posts");
-    //   const json = await response.json();
-    // }
-    // catchPost();
-     
- 
+  // USING ASYNC AND AWAIT FOR SAME CODE
+
+  // async function catchPost() {
+  //   const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  //   const json = await response.json();
+  // }
+  // catchPost();
 }
- consumeAPI();
+consumeAPI();
+
+//MANAGE ARTICLE SECTION
+function manageArticle() {
+  const userNameComment = document.querySelector("#userNameComment");
+  const userName = document.querySelector(".userName");
+  const submitComment = document.querySelector("#submit-comment");
+  const addComment = document.querySelector("#addComment");
+  const commentBody = document.querySelector(".comment-body");
+  const commentAndReply = document.querySelector(".comment-and-reply");
+  const commentSection = document.querySelector(".comment-section");
+  //FOR COMMENTING
+
+  //FOR COMMENTING
+  submitComment.addEventListener("click", submitWrittenComment);
+  //FOR COMMENTING
+
+  //FOR COMMENTING
+  function submitWrittenComment(e) {
+    e.preventDefault();
+
+    //create NEW comment-section div
+    if (userNameComment.value == false) {
+      alert("Enter Name Please");
+    } else if (addComment.value == false) {
+      alert("Please Enter Comment");
+    } else {
+      // let div = document.createElement("div")
+      // let p = document.createElement("p")
+      // div.append(p)
+
+      //create commentBoxforReactions div
+      let newUserName = document.createElement("div");
+      newUserName.className = "userName";
+
+      let newCommentBody = document.createElement("div");
+      newCommentBody.className = "comment-body";
+
+      let newcommentsCommentSectionReal = document.createElement("div");
+      newcommentsCommentSectionReal.className = "comments-section-real";
+
+      let newCommentParent = document.createElement("div");
+      newCommentParent.className = "commentBoxforReaction";
+
+      let newcommentAndReply = document.createElement("div");
+      newcommentAndReply.className = "comment-and-reply";
+
+      //create comments
+
+      newUserName.appendChild(
+        document.createTextNode(userNameComment.value.toUpperCase())
+      );
+      newCommentBody.appendChild(document.createTextNode(addComment.value));
+
+      newcommentsCommentSectionReal.append(newUserName);
+      newcommentsCommentSectionReal.append(newCommentBody);
+
+      newCommentParent.append(newcommentsCommentSectionReal);
+      newcommentAndReply.append(newCommentParent);
+
+      commentSection.appendChild(newcommentAndReply);
+    } //ADD TEXT NODE
+  }
+}
+
+manageArticle();
+
+function tooglePages() {
+  // pages
+  var indexSection = document.getElementById("index-section");
+  var adminSection = document.getElementById("admin-section");
+  var articleSection = document.getElementById("article-section");
+  // pages
+  console.log(indexSection.parentNode)
+
+  // pages buttons
+  var adminLogin = document.querySelector("#adminLogin");
+  var adminLogout = document.querySelector("#adminLogout");
+  // pages buttons
+
+  adminLogin.addEventListener("click", (e) => {
+     e.preventDefault();
+    indexSection.parentElement.replaceChild(adminSection, indexSection);
+    adminSection.classList.add(".active");
+    articleSection.classList.remove('.active');
+    
+  });
+
+  adminLogout.addEventListener("click", () => {});
+}
+tooglePages();
